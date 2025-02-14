14e20a1f197d5a812948228c409d327317ce83d0
nhlee98 <e5slnh9876@gmail.com>
2025-02-14 17:07:51 +0900

    feat: BadRequestException 추가

---
+++ src/main/java/weolbu/assignment/global/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package weolbu.assignment.global.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
